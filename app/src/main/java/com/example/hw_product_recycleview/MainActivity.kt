b4edd4f585acce222d30e37c490887171dc46a55
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2021-11-06 14:38:34 +0300

    Solution

--- app/src/main/java/com/example/hw_product_recycleview/MainActivity.kt
+++ app/src/main/java/com/example/hw_product_recycleview/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.hw_product_recycleview
 
+import adapter.ProductAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hw_product_recycleview.databinding.ActivityMainBinding
+import data.DataSource
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val data = DataSource().loadProduct()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ProductAdapter(this,data)
+        recyclerView.setHasFixedSize(true)
+
     }
 }
